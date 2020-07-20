a319ba302ccfae173e94c838a418e68181f03736
Matthew Nicoll <18638160+MDNicoll@users.noreply.github.com>
2020-07-19 21:13:33 -0400

    Created a basic title screen.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using LiteDB;
-using System;
+﻿using System;
 
 namespace RedElixir
 {
@@ -7,6 +6,27 @@ namespace RedElixir
     {
         static void Main(string[] args)
         {
+            // Basic setup & sizing for the console window
+            Console.SetWindowSize(81,30);
+            Console.SetBufferSize(81, 30);
+
+            WriteTitle();
+        }
+        private static void WriteTitle()
+        {
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(@" ================================================================================");
+            Console.WriteLine(@" =                      (                                                       ="); 
+            Console.WriteLine(@" =                      )\ )     (        (                                     ="); 
+            Console.WriteLine(@" =                     (()/(  (  )\ ) (   )\(     ) (  (                        ="); 
+            Console.WriteLine(@" =                      /(_))))\(()/( )\ ((_)\ ( /( )\ )(                       ="); 
+            Console.WriteLine(@" =                     (_)) /((_)((_)|(_) _((_))\()|(_|()\                      ="); 
+            Console.WriteLine(@" =                     | _ (_))  _| || __| |(_|(_)\ (_)((_)                     ="); 
+            Console.WriteLine(@" =                     |   / -_) _` || _|| || \ \ / | | '_|                     =");
+            Console.WriteLine(@" =                     |_|_\___\__,_||___|_||_/_\_\ |_|_|                       =");
+            Console.WriteLine(@" ================================================================================");
         }
     }
 }
