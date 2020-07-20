97745b2eabf7b4da9841402f28d56aa71aeeeb8e
Matthew Nicoll <18638160+MDNicoll@users.noreply.github.com>
2020-07-20 12:32:32 -0400

    Added a Screen Class.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Security.Cryptography.X509Certificates;
 
 namespace RedElixir
 {
@@ -7,68 +9,45 @@ namespace RedElixir
     {
         static void Main(string[] args)
         {
+            //---------------------------------------
+            // Basic console settings for the program
             Console.Title = "RedElixir - v1.0.0";
             Console.BackgroundColor = ConsoleColor.Black;
             Console.SetWindowSize(81,30);
             Console.SetBufferSize(81, 30);
             Console.CursorVisible = false;
 
-            do
+            //---------------------------------------
+            // Run the game!
+            Run();
+        }
+
+        private static void Run()
+        {
+            //---------------------------------------
+            // Run Class variables
+            string State = "";
+
+            //---------------------------------------
+            // Display the initial title screen
+            Screens.WriteTitle();
+
+            while (Console.ReadKey(true).Key != ConsoleKey.Q)
             {
-                WriteTitle();
+                switch (State)
+                {
+                    case "NewGame":
+                        Screens.WriteNewGame();
+                        break;
+
+                    default:
+                        Screens.WriteTitle();
+
+                        State = Console.ReadKey(true).Key == ConsoleKey.S ? "NewGame" : ""; 
+                        break;
+
+                }
             }
-            while (Console.ReadKey(true).Key != ConsoleKey.Q);
-        }
-
-        //---------------------------------------------------------------------------------------------------------
-        // Class WriteTitle() 
-        // -Writes a simple title screen for the game start
-        //
-        //---------------------------------------------------------------------------------------------------------
-        private static void WriteTitle()
-        {
-            Console.Clear();
-
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(@" ================================================================================");
-            Console.WriteLine(@" =                      (                                                       ="); 
-            Console.WriteLine(@" =                      )\ )     (        (                                     ="); 
-            Console.WriteLine(@" =                     (()/(  (  )\ ) (   )\(     ) (  (                        ="); 
-            Console.WriteLine(@" =                      /(_))))\(()/( )\ ((_)\ ( /( )\ )(                       ="); 
-            Console.WriteLine(@" =                     (_)) /((_)((_)|(_) _((_))\()|(_|()\                      ="); 
-            Console.WriteLine(@" =                     | _ (_))  _| || __| |(_|(_)\ (_)((_)                     ="); 
-            Console.WriteLine(@" =                     |   / -_) _` || _|| || \ \ / | | '_|                     =");
-            Console.WriteLine(@" =                     |_|_\___\__,_||___|_||_/_\_\ |_|_|                       =");
-            Console.WriteLine(@" ================================================================================");
-            Console.WriteLine(@" =                                                                              =");
-            Console.WriteLine(@" =                                                                              =");
-            Console.WriteLine(@" =                                                                              =");
-            Console.WriteLine(@" =  What would you like to do?                                                  =");
-            Console.WriteLine(@" = (S)tart a new game                                                           =");
-            Console.WriteLine(@" = (L)oad a saved game                                                          =");
-            Console.WriteLine(@" = (Q)uit the game                                                              =");
-            Console.WriteLine(@" =                                                                              =");
-            Console.WriteLine(@" =                                                                              =");
-            Console.WriteLine(@" =                                                                              =");
-            Console.WriteLine(@" =                                                                              =");
-            Console.WriteLine(@" =                                                                              =");
-            Console.WriteLine(@" =                                                                              =");
-            Console.WriteLine(@" =                                                                              =");
-            Console.WriteLine(@" =                                                                              =");
-            Console.WriteLine(@" =                                                                              =");
-            Console.WriteLine(@" =                                                                              =");
-            Console.WriteLine(@" =                                                                              =");
-            Console.WriteLine(@" ================================================================================");
-        }
-
-        //---------------------------------------------------------------------------------------------------------
-        // Class NewGame() 
-        // -Generates a new game save by: Creating a new XML file, filling that XML file with information for the 
-        // in-game character.
-        //---------------------------------------------------------------------------------------------------------
-        private static void NewGame()
-        {
-            Console.Clear();
         }
     }
 }
---
+++ Screens.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedElixir
+{
+    class Screens
+    {
+        public static void WriteTitle()
+        {
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(@" ================================================================================");
+            Console.WriteLine(@" =                      (                                                       =");
+            Console.WriteLine(@" =                      )\ )     (        (                                     =");
+            Console.WriteLine(@" =                     (()/(  (  )\ ) (   )\(     ) (  (                        =");
+            Console.WriteLine(@" =                      /(_))))\(()/( )\ ((_)\ ( /( )\ )(                       =");
+            Console.WriteLine(@" =                     (_)) /((_)((_)|(_) _((_))\()|(_|()\                      =");
+            Console.WriteLine(@" =                     | _ (_))  _| || __| |(_|(_)\ (_)((_)                     =");
+            Console.WriteLine(@" =                     |   / -_) _` || _|| || \ \ / | | '_|                     =");
+            Console.WriteLine(@" =                     |_|_\___\__,_||___|_||_/_\_\ |_|_|                       =");
+            Console.WriteLine(@" ================================================================================");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =  What would you like to do?                                                  =");
+            Console.WriteLine(@" = (S)tart a new game                                                           =");
+            Console.WriteLine(@" = (L)oad a saved game                                                          =");
+            Console.WriteLine(@" = (Q)uit the game                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" ================================================================================");
+        }
+
+        public static void WriteNewGame()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(@" ================================================================================");
+            Console.WriteLine(@" =                              CHARACTER CREATION                              =");
+            Console.WriteLine(@" ================================================================================");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" =                                                                              =");
+            Console.WriteLine(@" ================================================================================");
+        }
+    }
+}
